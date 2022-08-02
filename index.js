c14f7eb88604c04be367ab95bb4566582b7554e1
Juan Harvey Quintero Olarte <juan.harvey.quintero@accenture.com>
2022-08-02 16:07:37 -0500

    weather app with login option

---
+++ index.js
@@ -0,0 +1,30 @@
+
+//Load after de dom content is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  loginFunction();
+});
+function loginFunction() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        const user = document.getElementById("user").value;
+        const pwd = document.getElementById("pwd").value;
+        event.preventDefault();
+        if(user === logUser()[0].user && pwd === logUser()[0].pwd){
+            window.location.href = './pages/weather/weather.html'
+        }
+        else{
+            alert('invalid user')
+            form.reset();
+        }
+    });
+}
+
+const logUser = () => {
+    return [
+        {
+            "user" : "solvo",
+            "pwd" : "1234"
+        }
+    ]
+
+}
